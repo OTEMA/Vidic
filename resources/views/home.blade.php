4dcea03b8634c7de25e086e6cc534b2ae0bcbb07
TheOnlySmartBoy <o2jose43@gmail.com>
2017-10-09 12:46:43 +0300

    Begun adding new authentication for administrators

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -241,10 +241,10 @@
                                         <ul class="sidebar-menu">
                                             <li class="header">HEADER</li>
                                             <!-- Optionally, you can add icons to the links -->
-                                            <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-                                            <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+                                            <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Dash board</span></a></li>
+                                            <li><a href="#"><i class="fa fa-link"></i> <span>SUBSCRIPTIONS</span></a></li>
                                             <li class="treeview">
-                                                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+                                                <a href="#"><i class="fa fa-link"></i> <span></span>
                                                     <span class="pull-right-container">
                                                         <i class="fa fa-angle-left pull-right"></i>
                                                     </span>
