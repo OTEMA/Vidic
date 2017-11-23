b3b544aaf53276cb297feb615448650f933f297f
TheOnlySmartBoy <o2jose43@gmail.com>
2017-11-23 11:26:40 +0300

    Finished working on the admin login function

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,21 +4,40 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
-class AdminLoginController extends Controller
-{
+class AdminLoginController extends Controller {
+
+    protected $redirectTo = '/admin';
+    protected $guard = 'admin';
+
     public function __construct() {
-        $this->middleware('guest:admin');
+        $this->middleware('guest:admin')->except('logout');
     }
+
     public function showLoginForm() {
         return view('auth.admin-login');
     }
-    public function login(){
+
+    public function login(Request $request) {
         //validate the form data
-        //
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:9|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+        ]);
+
         // atttempt to log in the user
-        // 
-        // if successful redirect to their intetded location
-         // if unsucefull then redirect back to the login
+
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            // if successful redirect to their intetded location
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        // if unsucefull then redirect back to the login
+        return redirect()->back()->withInput($request->only('email','remember'));
     }
+
+    protected function guard() {
+        return \Auth::guard('admin');
+    }
+
 }
