a66ca096bc5c0129d8d9a0f8e8d4173be2c60729
TheOnlySmartBoy <o2jose43@gmail.com>
2017-11-23 11:02:51 +0300

    Restarted working on the admin login function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('admin.login') }}">Staff Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
