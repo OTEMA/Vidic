9df9c7e14881ddc85e34df8468f9b6e729dc1b4f
TheOnlySmartBoy <o2jose43@gmail.com>
2017-11-23 13:53:16 +0300

    Finished working on the admin login function

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -65,7 +65,7 @@ class Handler extends ExceptionHandler {
         }
         $guard = array_get($exception->guards(), 0);
         switch ($guard) {
-            case admin:
+            case 'admin':
                 $login = 'admin.login';
                 break;
             default :
